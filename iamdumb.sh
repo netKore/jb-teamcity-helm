77c7406eaa4f731021d8eb4c26559440f670f099
Your Name <you@example.com>
2025-05-14 17:18:05 +0000

    feat(dumbscript) fix

---
+++ iamdumb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOST="10.0.2.15"
+USER="postgres"
+PASSWORD="qazwsx"
+
+export PGPASSWORD=$PASSWORD
+kind delete cluster 
+rm -rf /root/www/*
+rm -rf /root/www/.teamcity
+psql --host=$HOST --username=$USER --dbname=postgres -c "DROP SCHEMA public CASCADE;"
+psql --host=$HOST --username=$USER --dbname=postgres -c "CREATE SCHEMA public;"
+kind create cluster --config=ha_kind  && git pull &&  helm install teamcity-ha --generate-name
+
+unset PGPASSWORD
+
+
