1898b2fda121c626f2c001b16e483822fe5c8d29
Alex <you@example.com>
2025-05-16 03:10:45 +0200

    feat(kind) fix

--- iamdumb.sh
+++ iamdumb.sh
@@ -10,7 +10,9 @@ rm -rf /root/www/*
 rm -rf /root/www/.teamcity
 psql --host=$HOST --username=$USER --dbname=postgres -c "DROP SCHEMA public CASCADE;"
 psql --host=$HOST --username=$USER --dbname=postgres -c "CREATE SCHEMA public;"
-kind create cluster --config=ha_kind  && git pull &&  helm install teamcity-ha --generate-name
+kind create cluster --config=ha_kind
+git pull
+helm install teamcity-ha --generate-name --set teamcity.vcs.password="$1"
 
 unset PGPASSWORD
 
